37d922219a67144f39307e83a3c981a8d7da34c3
daria-sukhareva <daria.sukhareva@shopify.com>
2025-08-22 13:59:28 -0400

    ocpp logs staging model
    ocpp logs staging model

---
+++ models/staging/ocpp_logs.sql
@@ -0,0 +1,48 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+with source as (
+    select * from {{ source('raw', 'ocpp_logs') }}
+),
+
+renamed as (
+    select
+        -- Convert timestamp to proper timestamp type using dbt macro for cross-platform compatibility
+        cast(timestamp as {{ dbt.type_timestamp() }}) as timestamp,
+        
+        -- Charge point identifier
+        cast(id as {{ dbt.type_string() }}) as charge_point_id,
+        
+        -- OCPP action type (can be null for response messages)
+        cast(action as {{ dbt.type_string() }}) as action,
+                
+        -- Extract MessageTypeId from the JSON array for easier filtering
+        -- Using fivetran_utils json_extract macro
+        {{ fivetran_utils.json_extract(string="msg", string_path=[0]) }} as message_type_id,
+        
+        -- Extract UniqueId from the JSON array
+        {{ fivetran_utils.json_extract(string="msg", string_path=[1]) }} as unique_id,
+        
+        -- Extract Action from the JSON array (only for request messages)
+        case 
+            when {{ fivetran_utils.json_extract(string="msg", string_path=[0]) }} = '2' 
+            then {{ fivetran_utils.json_extract(string="msg", string_path=[2]) }}
+            else null
+        end as extracted_action,
+        
+        -- Extract Payload from the JSON array
+        case 
+            when {{ fivetran_utils.json_extract(string="msg", string_path=[0]) }} = '2' 
+            then {{ fivetran_utils.json_extract(string="msg", string_path=[3]) }}
+            when {{ fivetran_utils.json_extract(string="msg", string_path=[0]) }} = '3' 
+            then {{ fivetran_utils.json_extract(string="msg", string_path=[2]) }}
+            else null
+        end as extracted_payload
+
+    from source
+)
+
+select * from renamed
