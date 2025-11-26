21be529994d6eb088014f50231bc0610fb9be107
daria-sukhareva <daria@kwwhat.com>
2025-11-26 11:01:24 -0500

    staging for ports
    staging for ports

---
+++ models/staging/stg_ports.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+with source as (
+    select * from {{ source('raw', 'ports') }}
+),
+
+renamed as (
+    select
+        cast(charger_id as {{ dbt.type_string() }}) as charger_id,
+        cast(port_id as {{ dbt.type_string() }}) as port_id,
+        cast(connector_id as {{ dbt.type_string() }}) as connector_id,
+        cast(connector_type as {{ dbt.type_string() }}) as connector_type,
+        cast(commissioned_ts as {{ dbt.type_timestamp() }}) as commissioned_ts,
+        cast(decommissioned_ts as {{ dbt.type_timestamp() }}) as decommissioned_ts
+    from source
+)
+
+select * from renamed
