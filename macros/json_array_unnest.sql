831648e9877dab1647b2f9a42ff6653b15cda3ee
Daria <daria@kwwhat.com>
2026-04-21 18:56:59 -0400

    adapter.dispatch refactoring (#84)
    adapter.dispatch refactoring + fixing lint line length issue
    and couple other readme fixes

--- macros/json_array_unnest.sql
+++ macros/json_array_unnest.sql
@@ -1,27 +1,51 @@
 {% macro json_array_unnest(json_column) %}
     {#
-      Cross-warehouse compatible unnest for JSON arrays
-      
-      Args:
-        json_column: The JSON array column to unnest
-      
-      Example:
-        cross join {{ json_array_unnest('meter_values') }}
+      Cross-warehouse compatible unnest for JSON arrays.
+      Example: cross join {{ json_array_unnest('meter_values') }}
     #}
-    {% if target.type == 'snowflake' %}
-        cross join lateral flatten(input => parse_json({{ json_column }}))
-    {% elif target.type == 'bigquery' %}
-        cross join unnest(json_extract_array({{ json_column }}))
-    {% elif target.type == 'duckdb' %}
-        cross join (select unnest(json_transform({{ json_column }}, '["JSON"]')) as value)
-    {% elif target.type in ['postgres', 'redshift'] %}
-        cross join unnest({{ json_column }})
-    {% elif target.type in ['spark', 'databricks'] %}
-        cross join lateral explode({{ json_column }})
-    {% elif target.type in ['trino', 'presto', 'athena'] %}
-        cross join unnest(json_extract_array({{ json_column }}, '$'))
-    {% else %}
-        -- Fallback for other warehouses - may need adjustment
-        cross join unnest({{ json_column }})
-    {% endif %}
+    {{ return(adapter.dispatch('json_array_unnest', 'kwwhat')(json_column)) }}
+{% endmacro %}
+
+{% macro default__json_array_unnest(json_column) %}
+    cross join unnest({{ json_column }})
+{% endmacro %}
+
+{% macro snowflake__json_array_unnest(json_column) %}
+    cross join lateral flatten(input => parse_json({{ json_column }}))
+{% endmacro %}
+
+{% macro bigquery__json_array_unnest(json_column) %}
+    cross join unnest(json_extract_array({{ json_column }}))
+{% endmacro %}
+
+{% macro duckdb__json_array_unnest(json_column) %}
+    cross join (select unnest(json_transform({{ json_column }}, '["JSON"]')) as value)
+{% endmacro %}
+
+{% macro postgres__json_array_unnest(json_column) %}
+    cross join unnest({{ json_column }})
+{% endmacro %}
+
+{% macro redshift__json_array_unnest(json_column) %}
+    cross join unnest({{ json_column }})
+{% endmacro %}
+
+{% macro spark__json_array_unnest(json_column) %}
+    cross join lateral explode({{ json_column }})
+{% endmacro %}
+
+{% macro databricks__json_array_unnest(json_column) %}
+    cross join lateral explode({{ json_column }})
+{% endmacro %}
+
+{% macro trino__json_array_unnest(json_column) %}
+    cross join unnest(json_extract_array({{ json_column }}, '$'))
+{% endmacro %}
+
+{% macro presto__json_array_unnest(json_column) %}
+    cross join unnest(json_extract_array({{ json_column }}, '$'))
+{% endmacro %}
+
+{% macro athena__json_array_unnest(json_column) %}
+    cross join unnest(json_extract_array({{ json_column }}, '$'))
 {% endmacro %}
