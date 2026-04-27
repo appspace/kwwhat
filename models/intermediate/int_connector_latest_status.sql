64fda59d6a30eafd6365a41a7cc3c27dc24a8719
Yegor <sr0rr3@gmail.com>
2026-04-28 01:15:23 +0300

    Change int_connector_latest_status model to incremental. Add processing only a data batch received after the last run

--- models/intermediate/int_connector_latest_status.sql
+++ models/intermediate/int_connector_latest_status.sql
@@ -1,6 +1,9 @@
 {{
   config(
-    materialized='view'
+    materialized='incremental',
+    unique_key=["charge_point_id", "connector_id"],
+    incremental_strategy="merge",
+    cluster_by="latest_status_ts"
   )
 }}
 
@@ -9,6 +12,13 @@ select
     connector_id,
     status as latest_status,
     error_code as latest_error_code,
-    ingested_ts as latest_status_ts
+    ingested_ts as latest_status_ts,
+    max(ingested_ts) over() as incremental_ts
 from {{ ref('int_status_changes') }}
 where next_status is null
+
+{%- if is_incremental() -%}
+    and ingested_ts >= (select max(incremental_ts) from {{ this }})
+{%- else -%}
+    and ingested_ts >= cast( '{{ var("start_processing_date") }}' as {{ dbt.type_timestamp() }})
+{%- endif -%}
