f3d7a87da0be7bc5e05675c7427f43959ff6e8cb
Yegor <sr0rr3@gmail.com>
2026-04-30 01:04:03 +0300

    Use incremental_date_range macro

--- models/intermediate/int_connector_latest_status.sql
+++ models/intermediate/int_connector_latest_status.sql
@@ -7,19 +7,38 @@
   )
 }}
 
+{%- if is_incremental() -%}
+    {%- set from_ts_caps = ["(select max(incremental_ts) from " ~ this ~ ")"] -%}
+{%- else -%}
+    {%- set from_ts_caps = [
+        "cast( '" ~ var("start_processing_date") ~ "' as " ~ dbt.type_timestamp() ~ ")"
+    ] -%}
+{%- endif -%}
+
+with incremental_date_range as (
+    {{ incremental_date_range(from_timestamp_caps=from_ts_caps, buffer_minutes=0) }}
+),
+    source_window as (
+        select
+            charge_point_id,
+            connector_id,
+            port_id,
+            status,
+            error_code,
+            ingested_ts,
+            max(ingested_ts) over() as incremental_ts
+        from {{ ref('int_status_changes') }}
+        cross join incremental_date_range
+        where next_status is null
+            and ingested_ts >= incremental_date_range.from_timestamp
+    )
+
 select
-    charge_point_id,
-    connector_id,
-    port_id,
+    charge_point_id,                                                                                                                                     
+    connector_id,   
+    port_id,     
     status as latest_status,
     error_code as latest_error_code,
-    ingested_ts as latest_status_ts,
-    max(ingested_ts) over() as incremental_ts
-from {{ ref('int_status_changes') }}
-where next_status is null
-
-{%- if is_incremental() -%}
-    and ingested_ts >= (select max(incremental_ts) from {{ this }})
-{%- else -%}
-    and ingested_ts >= cast( '{{ var("start_processing_date") }}' as {{ dbt.type_timestamp() }})
-{%- endif -%}
+    ingested_ts as latest_status_ts,                                                                                                                                    
+    incremental_ts
+from source_window
