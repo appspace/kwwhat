19324af3d4051c9debba95c0518849e308b7d1ee
Yegor <sr0rr3@gmail.com>
2026-05-19 02:28:32 +0300

    Drop first_seen_location_id as an unnecessary overhead

--- models/marts/dim_drivers.sql
+++ models/marts/dim_drivers.sql
@@ -11,22 +11,8 @@ with attempts as (
                 then cast({{ array_first('att.id_tags') }} as {{ dbt.type_string() }})
             else null
         end as id_tag,
-        att.charge_attempt_start_ts,
-        p.location_id
+        att.charge_attempt_start_ts
     from {{ ref('fact_charge_attempts') }} att
-    inner join {{ ref('stg_ports') }} p
-        on att.charge_point_id = p.charge_point_id
-        and att.connector_id = p.connector_id
-),
-
-attempts_ranked as (
-    select
-        *,
-        row_number() over (
-            partition by id_tag is null, id_tag
-            order by charge_attempt_start_ts
-        ) as rn_first_in_group
-    from attempts
 ),
 
 known_drivers as (
@@ -35,9 +21,8 @@ known_drivers as (
         id_tag,
         true as is_known_driver,
         min(charge_attempt_start_ts) as first_seen_ts,
-        max(charge_attempt_start_ts) as last_seen_ts,
-        min(case when rn_first_in_group = 1 then location_id end) as first_seen_location_id
-    from attempts_ranked
+        max(charge_attempt_start_ts) as last_seen_ts
+    from attempts
     where id_tag is not null
     group by id_tag
 ),
@@ -47,12 +32,9 @@ unknown_driver as (
         {{ dbt_utils.generate_surrogate_key(["'UNKNOWN'"]) }} as driver_key,
         'UNKNOWN' as id_tag,
         false as is_known_driver,
-        coalesce(min(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as
-first_seen_ts,
-        coalesce(max(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as
-last_seen_ts,
-        min(case when rn_first_in_group = 1 then location_id end) as first_seen_location_id
-    from attempts_ranked
+        coalesce(min(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as first_seen_ts,
+        coalesce(max(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as last_seen_ts
+    from attempts
     where id_tag is null
 )
 
@@ -61,8 +43,7 @@ select
     id_tag,
     is_known_driver,
     first_seen_ts,
-    last_seen_ts,
-    first_seen_location_id
+    last_seen_ts
 from known_drivers
 union all
 select
@@ -70,6 +51,5 @@ select
     id_tag,
     is_known_driver,
     first_seen_ts,
-    last_seen_ts,
-    first_seen_location_id
+    last_seen_ts
 from unknown_driver
