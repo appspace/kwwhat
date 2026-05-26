cce326c8279f72c71aba1bb3474ff42efbe26999
Yegor <sr0rr3@gmail.com>
2026-05-27 01:18:53 +0300

    Finish initial dim_drivers implemetation. Add tests, including a unit test. Add drivers to the semantic model

--- models/marts/dim_drivers.sql
+++ models/marts/dim_drivers.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    materialized='table',
+    materialized='table'
   )
 }}
 
@@ -11,6 +11,11 @@ with attempts as (
                 then cast({{ array_first('att.id_tags') }} as {{ dbt.type_string() }})
             else null
         end as id_tag,
+        case
+            when att.id_tag_statuses is not null and {{ array_size('att.id_tag_statuses') }} > 0
+                then cast({{ array_first('att.id_tag_statuses') }} as {{ dbt.type_string() }})
+            else null
+        end as id_tag_status,
         att.charge_attempt_start_ts
     from {{ ref('fact_charge_attempts') }} att
 ),
@@ -21,7 +26,9 @@ known_drivers as (
         id_tag,
         true as is_known_driver,
         min(charge_attempt_start_ts) as first_seen_ts,
-        max(charge_attempt_start_ts) as last_seen_ts
+        max(charge_attempt_start_ts) as last_seen_ts,
+        min_by(id_tag_status, charge_attempt_start_ts) as first_authorization_status,
+        max_by(id_tag_status, charge_attempt_start_ts) as latest_authorization_status
     from attempts
     where id_tag is not null
     group by id_tag
@@ -33,7 +40,9 @@ unknown_driver as (
         'UNKNOWN' as id_tag,
         false as is_known_driver,
         coalesce(min(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as first_seen_ts,
-        coalesce(max(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as last_seen_ts
+        coalesce(max(charge_attempt_start_ts), cast('1900-01-01' as {{ dbt.type_timestamp() }})) as last_seen_ts,
+        cast(null as {{ dbt.type_string() }}) as first_authorization_status,
+        cast(null as {{ dbt.type_string() }}) as latest_authorization_status
     from attempts
     where id_tag is null
 )
@@ -43,7 +52,9 @@ select
     id_tag,
     is_known_driver,
     first_seen_ts,
-    last_seen_ts
+    last_seen_ts,
+    first_authorization_status,
+    latest_authorization_status
 from known_drivers
 union all
 select
@@ -51,5 +62,7 @@ select
     id_tag,
     is_known_driver,
     first_seen_ts,
-    last_seen_ts
+    last_seen_ts,
+    first_authorization_status,
+    latest_authorization_status
 from unknown_driver
--- models/marts/dim_ports.sql
+++ models/marts/dim_ports.sql
@@ -1,7 +1,6 @@
 {{
   config(
-    materialized='table',
-    description='Port/connector dimension; full refresh from int_ports'
+    materialized='table'
   )
 }}
 
